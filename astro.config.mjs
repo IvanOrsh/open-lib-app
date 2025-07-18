6334b72662ebc533a736875115c9f50084fdc6c9
IvanOrsh <ksironov@gmail.com>
2025-07-18 15:32:52 +0300

    build: biomejs, tailwind, etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+
+import db from "@astrojs/db";
+
+import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	adapter: vercel(),
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
+
+	integrations: [db()],
+});
