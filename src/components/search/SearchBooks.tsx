dcf68bd4ff88ce368f4b6c335cf14feaae23b3a8
IvanOrsh <ksironov@gmail.com>
2025-07-18 21:11:31 +0300

    feat: search books - wip

---
+++ src/components/search/SearchBooks.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { actions } from "astro:actions";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Loader2, Save, SearchIcon } from "lucide-react";
+
+import type { ProcessedBook } from "@/types/Books";
+
+const SearchBooks = () => {
+	const [query, setQuery] = useState("");
+
+	const [books, setBooks] = useState<ProcessedBook[]>([]);
+
+	const [isLoading, setIsLoading] = useState(false);
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setIsLoading(true);
+
+		const { error, data } = await actions.searchBooks({ q: query });
+		if (error) {
+			console.error(error);
+			alert("Error searching for books");
+		}
+
+		if (data) {
+			setBooks(data as ProcessedBook[]);
+		}
+
+		setIsLoading(false);
+	};
+
+	return (
+		<>
+			<form className="grid gap-2 max-w-lg" onSubmit={handleSubmit}>
+				<Input
+					type="search"
+					name="q"
+					placeholder="Search for a book"
+					value={query}
+					onChange={(e) => setQuery(e.target.value)}
+				/>
+				<Button type="submit" disabled={isLoading}>
+					{isLoading ? (
+						<>
+							<span>Fetching books...</span>
+							<Loader2 className="w-4 h-4 animate-spin" />
+						</>
+					) : (
+						<>
+							<span>Search for books</span>
+							<SearchIcon className="w-4 h-4" />
+						</>
+					)}
+				</Button>
+			</form>
+			<div className="grid grid-cols-4 gap-4">
+				{books.map((book) => (
+					<div key={book.id} className="grid gap-2">
+						<h2 className="text-lg font-bold">{book.title}</h2>
+						<p className="text-sm text-gray-500">{book.author}</p>
+						{book.cover.url ? (
+							<img
+								src={book.cover.url}
+								alt={book.title}
+								width={book.cover.width}
+								height={book.cover.height}
+								className="h-60"
+							/>
+						) : (
+							<div className="h-60 bg-gray-200" />
+						)}
+						<Button variant="outline">
+							<Save className="w-4 h-4" />
+							<span>Save Book</span>
+						</Button>
+					</div>
+				))}
+			</div>
+		</>
+	);
+};
+
+export default SearchBooks;
---
+++ src/types/Books.ts
@@ -0,0 +1,38 @@
+export type ProcessedBook = {
+	title: string;
+	author: string[];
+	cover: {
+		url: string | undefined;
+		width: number;
+		height: number;
+	};
+	id: string;
+	ratings_average: number;
+};
+
+export type Book = {
+	cover_i: number;
+	has_fulltext: boolean;
+	edition_count: number;
+	title: string;
+	author_name: string[];
+	first_publish_year: number;
+	key: string;
+	ia: string[];
+	author_key: string[];
+	public_scan_b: boolean;
+	ratings_average: number | null;
+};
+
+export type BookResponse = {
+	start: number;
+	numFoundExact: boolean;
+	num_found: number;
+	docs: Book[];
+};
+
+export type CoverResponse = {
+	source_url: string | null;
+	width: number;
+	height: number;
+};
