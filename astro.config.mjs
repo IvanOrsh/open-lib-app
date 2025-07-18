380012a8308d62ba2db1fc8fb5f69090c0038145
IvanOrsh <ksironov@gmail.com>
2025-07-18 15:57:06 +0300

    chore: add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,15 @@ import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	adapter: vercel(),
+    adapter: vercel(),
 
-	vite: {
-		plugins: [tailwindcss()],
-	},
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-	integrations: [db()],
+    integrations: [db(), react()],
 });
